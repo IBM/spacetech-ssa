67e44932e875538dd7aebf0b55dc867334360c0f
Colin Alstad <colin.alstad@ibm.com>
2020-11-12 15:13:01 -0500

    Move commonly used function to be available in the package

--- orbit_prediction/orbit_prediction/__init__.py
+++ orbit_prediction/orbit_prediction/__init__.py
@@ -11,3 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import itertools
+
+
+def get_state_vect_cols(prefix=''):
+    """Get the column names of the state vector components with the
+    provided `prefix`.
+
+    :param prefix: The prefix that is used in front of the state vector
+        components in the column names, examples are `physics_pred` and
+        `physics_err` or none
+    :type prefix: str
+
+    :return: A list of the 6 names of the prefixed state vector components
+    :rtype: [str]
+    """
+    if prefix:
+        prefix += '_'
+    vectors = ['r', 'v']
+    components = ['x', 'y', 'z']
+    col_names = [f'{prefix}{v}_{c}'
+                 for v, c
+                 in itertools.product(vectors, components)]
+    return col_names
