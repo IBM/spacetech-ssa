0c56a6389677000e018a899146f807c0d5f4157f
Colin Alstad <colin.alstad@ibm.com>
2020-11-12 16:36:28 -0500

    Parallelize physics model

--- orbit_prediction/orbit_prediction/physics_model.py
+++ orbit_prediction/orbit_prediction/physics_model.py
@@ -14,6 +14,7 @@
 
 # Numeric/ML libraries
 import numpy as np
+from joblib import Parallel, delayed
 from sklearn.base import BaseEstimator
 # Astrodynamics libraries
 from astropy import units as u
@@ -25,8 +26,35 @@ from poliastro.core.perturbations import J2_perturbation
 
 
 class PhysicsModel(BaseEstimator):
+    """A simple model for orbit propagation implementing the
+    scikit-learn API
+
+    :param n_jobs: The number of processors to use in predicting orbtis
+    :type n_jobs: int
+    """
+
+    def __init__(self, n_jobs=1):
+        self.n_jobs = 1
+
+    def _fit_row(self, row):
+        """Creates an Orbit object based on the state values in `row`.
+
+        :param row: An array containing the epoch and state vectors to
+            instantiate the orbit at
+        :type row: np.array
+
+        :return: An Orbit object instantiated at the given state
+        :rtype: poliastro.twobody.Orbit
+        """
+        epoch = Time(row[0], scale='utc')
+        r = row[1:4] * u.m
+        v = row[4:7] * (u.m / u.s)
+        orbit = Orbit.from_vectors(Earth, r, v, epoch=epoch)
+        return orbit
+
     def fit(self, time_state_vectors):
         """Constructs orbit objects for each row of the provided matrix.
+
         :param time_state_vectors: A numpy array of shape (n, 7) where each
             row has the following data in these indexes:
                 0 - epoch at which to instantiate the orbit
@@ -35,14 +63,50 @@ class PhysicsModel(BaseEstimator):
             This data is then used to create `n` orbit objects.
         :type time_state_vectors: numpy.ndarray
         """
-        orbits = []
-        for row in time_state_vectors:
-            epoch = Time(row[0], scale='utc')
-            r = row[1:4] * u.m
-            v = row[4:7] * (u.m / u.s)
-            orbit = Orbit.from_vectors(Earth, r, v, epoch=epoch)
-            orbits.append(orbit)
+        orbits = Parallel(n_jobs=self.n_jobs)(delayed(self._fit_row)(row)
+                                              for row in time_state_vectors)
         self.orbits = orbits
+        return self
+
+    def _predict(self, orbit, time_array):
+        """Predicts the orbital state vectors for the given orbit for each
+        provided time.
+
+        :param orbit: The orbit to predict future state vectors of
+        :type orbit: poliastro.twobody.Orbit
+
+        :param time_array: Array of seconds in the future to propagate the
+            orbit to
+        :type time_array: np.array
+
+        :return: A list of predicted state vectors for each provided time
+        :rtype: list
+        """
+        def propagate(s):
+            """Propagates the closed over orbit object to `s` seconds into
+            the future.
+
+            :param s: Seconds into the future to propagate the orbit to
+            :type s: float
+
+            :return: The predicted state vectors of the orbit at `s` seconds
+                in the future
+            :rtype: np.array
+            """
+            prop_orbit = orbit.propagate(s*u.s,
+                                         method=cowell,
+                                         ad=J2_perturbation,
+                                         J2=Earth.J2.value,
+                                         R=Earth.R.to(u.km).value)
+            prop_r, prop_v = prop_orbit.rv()
+            prop_state_vect = np.concatenate(
+                [prop_r.to(u.m).to_value(),
+                 prop_v.to(u.m/u.s).to_value()]
+            )
+            return prop_state_vect
+
+        state_vects = [propagate(s) for s in time_array]
+        return state_vects
 
     def predict(self, times):
         """Propagates the fitted orbits to the provided times.
@@ -57,20 +121,7 @@ class PhysicsModel(BaseEstimator):
             in `times`
         :rtype: numpy.ndarray
         """
-        orbit_vects = []
-        for orbit, time_array in zip(self.orbits, times):
-            state_vects = []
-            for elapsed_seconds in time_array:
-                prop_orbit = orbit.propagate(elapsed_seconds*u.s,
-                                             method=cowell,
-                                             ad=J2_perturbation,
-                                             J2=Earth.J2.value,
-                                             R=Earth.R.to(u.km).value)
-                prop_r, prop_v = prop_orbit.rv()
-                prop_state_vect = np.concatenate(
-                    [prop_r.to(u.m).to_value(),
-                     prop_v.to(u.m/u.s).to_value()]
-                )
-                state_vects.append(prop_state_vect)
-            orbit_vects.append(state_vects)
+        orbit_times = zip(self.orbits, times)
+        orbit_vects = Parallel(n_jobs=self.n_jobs)(delayed(self._predict)(o, t)
+                                                   for (o, t) in orbit_times)
         return np.array(orbit_vects)
